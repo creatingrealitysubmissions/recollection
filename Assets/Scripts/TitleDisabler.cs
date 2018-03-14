35d736a8c3803634a78eb6241bf575af99fe14ea
Ljferrer <lukasjferrer@gmail.com>
2018-03-14 15:48:37 -0700

    Fader
    Intro fades out

---
+++ Assets/Scripts/TitleDisabler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleDisabler : MonoBehaviour 
+{
+    // Start screen black and fade into title 
+    public float FadeDelay;
+    public float FadeTime;
+    float t = 0f;
+
+
+    IEnumerator Start()
+    {
+        // Wait for suspense 
+        yield return new WaitForSeconds(FadeDelay);
+
+        this.gameObject.SetActive(false);
+    }
+}
