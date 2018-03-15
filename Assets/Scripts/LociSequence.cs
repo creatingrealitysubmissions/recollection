98ef62a8cde601a0a117aac55bf327b4f0139535
Ljferrer <lukasjferrer@gmail.com>
2018-03-14 17:00:53 -0700

    Final Build Complete
    Had to modify the way Game Objects were instantiated

--- Assets/Scripts/LociSequence.cs
+++ Assets/Scripts/LociSequence.cs
@@ -14,102 +14,103 @@ public class LociSequence : MonoBehaviour
     // Ensure player doesn't put all items in same place
     public static float Distance;
     public static int NewLocation = 1;
-    Vector3 LastLocation;
-    int TapCount = 0;
+    private Vector3 LastLocation;
+    private int TapCount = 0;
 
 
     void Update()
     {
-        MovementChecker(Camera.main.transform.position);
-
+        MovementChecker(PoV.transform.position);
         if (Input.touchCount > 0)
         {
-            Spawner();
+            // Spawn when screen is tapped
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Define object position and rotation relative to camera 
+                Vector3 newObjectPosition = PoV.transform.position;
+                newObjectPosition.z += 0.5f;
+
+                var TurnTo = PoV.transform.position - newObjectPosition;
+
+                // Sorry for the spagetti conditions... new to C#
+                // Note: Must code spawning in reverse order
+                if (TapCount == 4 && NewLocation == 1)
+                {
+                    // Spawn
+                    Sentence5.gameObject.SetActive(true);
+                    GameObject Poop = Instantiate(Sentence5, newObjectPosition,
+                                                  Quaternion.identity) as GameObject;
+                    Poop.transform.rotation = Quaternion.LookRotation(TurnTo);
+
+                    // Track
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+                if (TapCount == 3 && NewLocation == 1)
+                {
+                    // Spawn
+                    Sentence4.gameObject.SetActive(true);
+                    GameObject Hunnid = Instantiate(Sentence4, newObjectPosition,
+                                                    Quaternion.identity) as GameObject;
+                    Hunnid.transform.rotation = Quaternion.LookRotation(TurnTo);
+
+                    // Track
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+                if (TapCount == 2 && NewLocation == 1)
+                {
+                    // Spawn
+                    Sentence3.gameObject.SetActive(true);
+                    GameObject Wine = Instantiate(Sentence3, newObjectPosition,
+                                                  Quaternion.identity) as GameObject;
+                    Wine.transform.rotation = Quaternion.LookRotation(TurnTo);
+
+                    // Track
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+                if (TapCount == 1 && NewLocation == 1)
+                {
+                    // Spawn
+                    Sentence2.gameObject.SetActive(true);
+                    GameObject House = Instantiate(Sentence2, newObjectPosition,
+                                                   Quaternion.identity) as GameObject;
+                    House.transform.rotation = Quaternion.LookRotation(TurnTo);
+
+                    // Track
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+                if (TapCount == 0 && NewLocation == 1)
+                {
+                    // Spawn
+                    Sentence1.gameObject.SetActive(true);
+                    GameObject Thinking = Instantiate(Sentence1, newObjectPosition,
+                                                      Quaternion.identity) as GameObject;
+                    Thinking.transform.rotation = Quaternion.LookRotation(TurnTo);
+
+                    // Track
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+            }
         }
     }
 
 
     // Spawn sequence of objects
-    void Spawner()
-    {
-        // Spawn when screen is tapped
-        var touch = Input.GetTouch(0);
-        if (touch.phase == TouchPhase.Began)
-        {
-            // Define object position and rotation relative to camera 
-            Vector3 newObjectPosition = PoV.transform.position;
-            newObjectPosition.z += 0.5f;
-
-            var TurnTo = PoV.transform.position - newObjectPosition;
-
-            // Sorry for the spagetti conditions... new to C#
-            // Note: Must code spawning in reverse order
-            if (TapCount == 4 && NewLocation == 1)
-            {
-                // Spawn
-                GameObject Poop = Instantiate(Sentence5, newObjectPosition, 
-                                              Quaternion.identity) as GameObject;
-                Poop.transform.rotation = Quaternion.LookRotation(TurnTo);
-
-                // Track
-                TapCount += 1;
-                LastLocation = PoV.transform.position;
-                NewLocation = 0;
-            }
-
-            if (TapCount == 3 && NewLocation == 1)
-            {
-                // Spawn
-                GameObject Hunnid = Instantiate(Sentence4, newObjectPosition, 
-                                                Quaternion.identity) as GameObject;
-                Hunnid.transform.rotation = Quaternion.LookRotation(TurnTo);
-
-                // Track
-                TapCount += 1;
-                LastLocation = PoV.transform.position;
-                NewLocation = 0;
-            }
-
-            if (TapCount == 2 && NewLocation == 1)
-            {
-                // Spawn
-                GameObject Wine = Instantiate(Sentence3, newObjectPosition, 
-                                              Quaternion.identity) as GameObject;
-                Wine.transform.rotation = Quaternion.LookRotation(TurnTo);
-
-                // Track
-                TapCount += 1;
-                LastLocation = PoV.transform.position;
-                NewLocation = 0;
-            }
-
-            if (TapCount == 1 && NewLocation == 1)
-            {
-                // Spawn
-                GameObject House = Instantiate(Sentence2, newObjectPosition, 
-                                               Quaternion.identity) as GameObject;
-                House.transform.rotation = Quaternion.LookRotation(TurnTo);
-
-                // Track
-                TapCount += 1;
-                LastLocation = PoV.transform.position;
-                NewLocation = 0;
-            }
-
-            if (TapCount == 0 && NewLocation == 1)
-            {
-                // Spawn
-                GameObject Thinking = Instantiate(Sentence1, newObjectPosition, 
-                                                  Quaternion.identity) as GameObject;
-                Thinking.transform.rotation = Quaternion.LookRotation(TurnTo);
-
-                // Track
-                TapCount += 1;
-                LastLocation = PoV.transform.position;
-                NewLocation = 0;
-            }
-        }
-    }
+    //void Spawner(){}
 
 
     // Track how far the player has moved since they placed the last object
---
+++ Assets/Scripts/LociSequence2.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class LociSequence2 : MonoBehaviour
+{
+    // Objects to activate
+    public GameObject Sentence1;
+    public GameObject Sentence2;
+    public GameObject Sentence3;
+    public GameObject Sentence4;
+    public GameObject Sentence5;
+    public GameObject PoV;
+
+    // Ensure player doesn't put all items in same place
+    public static float Distance;
+    public static int NewLocation = 1;
+    Vector3 LastLocation;
+    int TapCount = 0;
+
+
+    void Update()
+    {
+        MovementChecker(PoV.transform.position);
+
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Define object position and rotation relative to camera
+                var newObjectPosition = Camera.main.ScreenToWorldPoint(
+                    new Vector3(touch.position.x, touch.position.y, 3.0f));
+
+                var newObjectRotation = Quaternion.LookRotation(PoV.transform.forward);
+                newObjectRotation *= Quaternion.Euler(Vector3.up * 180);
+
+                // Note: Must code spawning in reverse order
+                if (TapCount == 4 && NewLocation == 1)
+                {
+                    Instantiate(Sentence5, newObjectPosition, newObjectRotation);
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+                if (TapCount == 3 && NewLocation == 1)
+                {
+                    Instantiate(Sentence4, newObjectPosition, newObjectRotation);
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+                if (TapCount == 2 && NewLocation == 1)
+                {
+                    Instantiate(Sentence3, newObjectPosition, newObjectRotation);
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+                if (TapCount == 1 && NewLocation == 1)
+                {
+                    Instantiate(Sentence2, newObjectPosition, newObjectRotation);
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+                if (TapCount == 0 && NewLocation == 1)
+                {
+                    Instantiate(Sentence1, newObjectPosition, newObjectRotation);
+                    TapCount += 1;
+                    LastLocation = PoV.transform.position;
+                    NewLocation = 0;
+                }
+
+            }
+        }
+    }
+
+
+    // Track how far the player has moved since they placed the last object
+    // If player has moved sufficiently far, unlock ability to place new object
+    void MovementChecker(Vector3 CurrentSpot)
+    {
+        if (NewLocation == 0)
+        {
+            Distance = Vector3.Distance(CurrentSpot, LastLocation);
+            if (Distance > 1.25f)
+            {
+                NewLocation = 1;
+            }
+        }
+    }
+
+
+}
